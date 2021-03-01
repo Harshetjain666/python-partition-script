6d76d386c1cd11813572a373df840cb98ca991b0
Ajay Pathak <ajaypathak372@gmail.com>
2021-03-01 18:17:27 +0530

    created partitions

--- partition.py
+++ partition.py
@@ -6,6 +6,8 @@ def mountpoint(disk_name):
 	os.system(f"mount /dev/{disk_name} {mount}")
 
 def format_type(disk_name):
+				os.system("tput bold")
+				os.system("tput setaf 2")
 				print("""Choose The Format Type
 				Press 1. For ext2
 				Press 2. For ext3
@@ -13,20 +15,182 @@ def format_type(disk_name):
 				format = int(input("Enter your Choice: "))
 
 				if format == 1:
-					os.system(f"mkfs.ext2 /dev/{disk_name} > /dev/null")
+					os.system(f"mkfs.ext2 /dev/{disk_name}1 > /dev/null")
 					os.system("sleep 3")
 					os.system("clear")
 					mountpoint(disk_name)
 				if format == 2:
-					os.system(f"mkfs.ext3 /dev/{disk_name} > /dev/null")
+					os.system(f"mkfs.ext3 /dev/{disk_name}1 > /dev/null")
 					os.system("sleep 3")
 					os.system("clear")
 					mountpoint(disk_name)
 				if format == 3:
-					os.system(f"mkfs.ext4 /dev/{disk_name} > /dev/null")
+					os.system(f"mkfs.ext4 /dev/{disk_name}1 > /dev/null")
 					os.system("sleep 3")
 					os.system("clear")
 					mountpoint(disk_name)
+
+def create_partition():
+	while True:
+		os.system("clear")
+		print("""Create a New Partition
+		Press 1. Create Static Partition
+		Press 2. Create LVM Partition
+		Press 3. Go To Main Menu
+		Press 4. To Exit
+		""")
+		option2_1 = int(input("Enter Your Choice: "))
+
+		if option2_1 == 1:
+			os.system("clear")
+			os.system("fdisk -l")
+			disk_name = input("Enter your disk name(e.g-sda): ")
+			os.system(f"fdisk /dev/{disk_name}")
+			static_options = input("Have you created the Partion[y/N]: ")  
+			#partition_name = input("Enter name of the partition you created(or if not created, leave it empty): ")
+			if static_options == 'N':
+				continue
+			elif static_options == 'y':
+				format_type(disk_name)
+		
+		elif option2_1 == 2:
+			os.system("clear")
+			os.system("fdisk -l")
+			disk_name = input("Enter your disk name(e.g-sda): ")
+			os.system("pvcreate /dev/{disk_name} > /dev/null")
+			vg_name = input("Enter name of VG: ")
+			os.system("vgcreate {vg_name} {disk_name} > /dev/null")
+			lvm_name = input("Enter your LVM name: ")
+			lvm_size = input("Enter size of LVM: ")
+			os.system(f"lvcreate --size {lvm_size} --name {lvm_name} {vg_name}")
+			format_type(disk_name)
+				
+		elif option2_1 == 3:
+			break
+		elif option2_1 == 4:
+			exit()
+		else:
+			print("Wrong Output\nTry again...\n")
+			continue
+
+def change_partitions():
+	while True:
+		os.system("clear")
+		print(""" Add or Delete Storage in Existing Partition
+		Press 1. Add Storage in Static Partition
+		Press 2. Increase or Decrease Storage in LVM
+		Press 3. Go To Main Menu
+		Press 4. Exit
+		""")
+		option3_1 = int(input("Enter Your Choice: "))
+
+		if option3_1 == 1:
+			os.system("clear")
+			os.system("fdisk -l")
+			print("\nPlease Make Sure Not To Remove The Signature")
+			disk_name = input("Enter your disk name(e.g-sda): ")
+			os.system(f"fdisk /dev/{disk_name}")
+			os.system(f"resize2fs /dev/{disk_name} > /dev/null")
+
+		elif option3_1 == 2:
+			os.system("clear")
+			print(""" Increase or Decrease Storage in LVM
+			Press 1. Increase LVM Partition Size
+			Press 2. Decrease LVM Partition Size
+			Press 3. Back
+			""")
+			option3_1_2 = int(input("Enter Your Choice: "))
+
+			if option3_1_2 == 1:
+				os.system("clear")
+				print(""" Increase LVM Storage in Linux
+				Press 1: Increase VG Size
+				Press 2: Increase LV Size
+				Press 3: Back
+				""")
+				lvm_options = int(input("Enter Your Choice: "))
+
+				if lvm_options == 1:
+					os.system("clear")
+					pv = input("Do You Have a PV?(Y or N): ")
+						
+					if pv == 'Y':
+						os.system("clear")
+						os.system("pvdisplay")
+						pv = input("Enter Your PV Name: ")
+						os.system("vgdisplay")	
+						vg = input("Enter Your VG Name: ")
+						os.system(f"vgextend {vg} {pv}")
+
+					elif pv == 'N':
+						os.system("clear")
+						os.system("fdisk -l")
+						disk_name = input("Enter your disk name(e.g-sda): ")
+						os.system("pvcreate /dev/{disk_name} > /dev/null")
+						os.system("vgdisplay")	
+						vg = input("Enter Your VG Name: ")
+						os.system(f"vgextend {vg} {disk_name}")
+
+					else:
+						print("Wrong Output\nTry again...\n")
+
+				elif lvm_options == 2:
+					os.system("clear")
+					os.system("vgdisplay")
+					vg = input("Enter Your VG Name: ")
+					os.system("lvdisplay")
+					lv = input("Enter Your LV Name: ")
+					lvm_size = input("Enter increased size of LVM: ")
+					os.system("lvextent -r --size +{lvm_size} /dev/{vg}/{lv}")
+						
+			elif option3_1_2 == 2:
+				if option3_1_2 == 1:
+					os.system("clear")
+					print(""" Decrease LVM Storage in Linux
+					Press 1: Decrease VG Size
+					Press 2: Decrease LV Size
+					""")
+					lvm_options = int(input("Enter Your Choice: "))
+
+					if lvm_options == 1:
+						os.system("clear")
+						os.system("tput setaf 1")
+						print("Warning: Thats Not a Good Practice Make Sure You First Create a Backup Before Do this")
+						os.system("tput setaf 7 ")
+						input("Enter To Continue...")
+						os.system("clear")
+						print(""" Note: There is some points remember before do this 
+						1. Make sure you have attached atleast 2 PV in a VG.
+						2. One of them is capable to copy all of the data of other.
+						""")
+						os.system("pvs -o +pv_used")
+						vg_name = input("Enter your VG name: ")
+						pv_remove = input("Enter your PV name which you want to remove: ")
+						os.system("pvmove --alloc anywhere {pv_remove}> /dev/null")
+						os.system("vgremove {vg_name} {pv_remove}")
+
+
+					elif lvm_options == 2:
+						os.system("clear")
+						os.system("tput setaf 1")
+						print("Warning: Thats Not a Good Practice Make Sure You First Create a Backup Before Do this")
+						os.system("tput setaf 7 ")
+						input("Enter To Continue...")
+						os.system("clear")
+						os.system("lvdisplay")
+						lv_path = input("Enter Your LV path: ")	
+						lv_size = int(input("Enter Your Final Size: "))
+						os.system(f"lvreduce -r --size {lv_size} {lv_path}")
+			if option3_1_2 == 3:
+				continue
+							
+		elif option3_1 == 3:
+			break
+		elif option3_1 == 4:
+			exit()
+		else:
+			print("Wrong Output\nTry again...\n")
+			continue
 
 
 def partition():
@@ -47,161 +211,10 @@ def partition():
 			input("\nPress any key to continue...")
 			os.system("clear")
 		elif channel == 2:
-			while True:
-				os.system("clear")
-				print("""Create a New Partition
-				Press 1. Create Static Partition
-				Press 2. Create LVM Partition
-				Press 3. Go To Main Menu
-				Press 4. To Exit
-				""")
-				option2_1 = int(input("Enter Your Choice: "))
-
-				if option2_1 == 1:
-					os.system("clear")
-					os.system("fdisk -l")
-					disk_name = input("Enter your disk name(e.g-sda): ")
-					os.system(f"fdisk /dev/{disk_name}")
-					partition_name = input("Enter name of the partition you created(or if not created, leave it empty): ")
-					if partition_name == '':
-						continue
-					else:
-						format_type(partition_name)
-		
-				elif option2_1 == 2:
-					os.system("clear")
-					os.system("fdisk -l")
-					disk_name = input("Enter your disk name(e.g-sda): ")
-					os.system("pvcreate /dev/{disk_name} > /dev/null")
-					vg_name = input("Enter name of VG: ")
-					os.system("vgcreate {vg_name} {disk_name} > /dev/null")
-					lvm_name = input("Enter your LVM name: ")
-					lvm_size = input("Enter size of LVM: ")
-					os.system(f"lvcreate --size {lvm_size} --name {lvm_name} {vg_name}")
-					format_type(disk_name)
-				
-				elif option2_1 == 3:
-					break
-				elif option2_1 == 4:
-					exit()
-
-				else:
-					print("Wrong Output\nTry again...\n")
-
-			if option2_1 == 4:
-				print("bye")
-				exit()
-
+			create_partition()
 
 		elif channel == 3:
-			os.system("clear")
-			print(""" Add or Delete Storage in Existing Partition
-			Press 1. Add Storage in Static Partition
-			Press 2. Increase or Decrease Storage in LVM
-			Press 3. Go To Main Menu
-			""")
-			option3_1 = int(input("Enter Your Choice: "))
-
-			if option3_1 == 1:
-				os.system("clear")
-				os.system("fdisk -l")
-				print("\nPlease Make Sure Not To Remove The Signature")
-				disk_name = input("Enter your disk name(e.g-sda): ")
-				os.system(f"fdisk /dev/{disk_name}")
-				os.system(f"resize2fs /dev/{disk_name} > /dev/null")
-
-			elif option3_1 == 2:
-				os.system("clear")
-				print(""" Increase or Decrease Storage in LVM
-				Press 1. Increase LVM Partition Size
-				Press 2. Decrease LVM Partition Size
-				""")
-				option3_1_2 = int(input("Enter Your Choice: "))
-
-				if option3_1_2 == 1:
-					os.system("clear")
-					print(""" Increase LVM Storage in Linux
-					Press 1: Increase VG Size
-					Press 2: Increase LV Size
-					""")
-					lvm_options = int(input("Enter Your Choice: "))
-
-					if lvm_options == 1:
-						os.system("clear")
-						pv = input("Do You Have a PV?(Y or N): ")
-						
-						if pv == 'Y':
-							os.system("clear")
-							os.system("pvdisplay")
-							pv = input("Enter Your PV Name: ")
-							os.system("vgdisplay")	
-							vg = input("Enter Your VG Name: ")
-							os.system(f"vgextend {vg} {pv}")
-
-						elif pv == 'N':
-							os.system("clear")
-							os.system("fdisk -l")
-							disk_name = input("Enter your disk name(e.g-sda): ")
-							os.system("pvcreate /dev/{disk_name} > /dev/null")
-							os.system("vgdisplay")	
-							vg = input("Enter Your VG Name: ")
-							os.system(f"vgextend {vg} {disk_name}")
-
-						else:
-							print("Wrong Output\nTry again...\n")
-
-					elif lvm_options == 2:
-						os.system("clear")
-						os.system("vgdisplay")
-						vg = input("Enter Your VG Name: ")
-						os.system("lvdisplay")
-						lv = input("Enter Your LV Name: ")
-						lvm_size = input("Enter increased size of LVM: ")
-						os.system("lvextent -r --size +{lvm_size} /dev/{vg}/{lv}")
-						
-
-				elif option3_1_2 == 2:
-					if option3_1_2 == 1:
-						os.system("clear")
-						print(""" Decrease LVM Storage in Linux
-						Press 1: Decrease VG Size
-						Press 2: Decrease LV Size
-						""")
-						lvm_options = int(input("Enter Your Choice: "))
-
-						if lvm_options == 1:
-							os.system("clear")
-							os.system("tput setaf 1")
-							print("Warning: Thats Not a Good Practice Make Sure You First Create a Backup Before Do this")
-							os.system("tput setaf 7 ")
-							input("Enter To Continue...")
-							os.system("clear")
-							print(""" Note: There is some points remember before do this 
-							1. Make sure you have attached atleast 2 PV in a VG.
-							2. One of them is capable to copy all of the data of other.
-							""")
-							os.system("pvs -o +pv_used")
-							vg_name = input("Enter your VG name: ")
-							pv_remove = input("Enter your PV name which you want to remove: ")
-							os.system("pvmove --alloc anywhere {pv_remove}> /dev/null")
-							os.system("vgremove {vg_name} {pv_remove}")
-
-
-						elif lvm_options == 2:
-							os.system("clear")
-							os.system("tput setaf 1")
-							print("Warning: Thats Not a Good Practice Make Sure You First Create a Backup Before Do this")
-							os.system("tput setaf 7 ")
-							input("Enter To Continue...")
-							os.system("clear")
-							os.system("lvdisplay")
-							lv_path = input("Enter Your LV path: ")
-							lv_size = int(input("Enter Your Final Size: "))
-							os.system(f"lvreduce -r --size {lv_size} {lv_path}")
-							
-			elif option3_1 == 3:
-				continue
-				os.system("clear")	
+			change_partitions()
 				
 		elif channel == 4:
 			os.system("clear")
@@ -214,7 +227,7 @@ def partition():
 
 		elif channel == 6:
 			print("bye")
-			exit
+			exit()
 
 		else:
 			print("Wrong Output\nTry again...\n")
